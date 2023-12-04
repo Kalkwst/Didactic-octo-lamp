fa56b970ae1306f78dda185927cf132cc0eafd0c
Konstantinos Kalafatis <kostas.kalafatis@sitecore.com>
2023-12-04 15:36:39 +0200

    Initial commit 2

---
+++ WorkerService1/Worker.cs
@@ -0,0 +1,29 @@
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WorkerService1
+{
+    public class Worker : BackgroundService
+    {
+        private readonly ILogger<Worker> _logger;
+
+        public Worker(ILogger<Worker> logger)
+        {
+            _logger = logger;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+
+                // Simulate some work
+                await Task.Delay(5000, stoppingToken);
+            }
+        }
+    }
+}
