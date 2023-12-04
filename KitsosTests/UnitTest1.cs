4f2fa614c2b27849c64eb9d31f74711f084fd2af
Konstantinos Kalafatis <kostas.kalafatis@sitecore.com>
2023-12-04 15:27:20 +0200

    Initial commit

---
+++ KitsosTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace KitsosTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
